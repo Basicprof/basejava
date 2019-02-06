d1bb67c5fdcde971906f4cad4d8895a102c79580
IHOR PETROV <Basicprof@gmail.com>
2019-02-06 16:57:02 +0200

    Домашнее задание HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+//        for (int i = 0; i <storage.length ; i++) {
+//            if(storage[i] == null) {storage[i] = r;break;}
+//        }
+//
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) break;
+            if (storage[i].toString() == uuid) {
+                resume = storage[i];
+                break;
+            }
+        }
+        if (resume == null) resume = new Resume();
+        resume.uuid = "not in the database";
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].toString() == uuid) {
+                storage[i] = null;
+                for (int j = i; j < storage.length - 2; j++) {
+                    storage[j] = storage[j + 1];
+                    if (storage[j] == null) {
+                        break;
+                    }
+                }
+                storage[storage.length - 1] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        List list = new ArrayList();
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) list.add(storage[i]);
+        }
+        Resume[] resumes = new Resume[list.size()];
+        list.toArray(resumes);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) count++;
+        }
+        return count;
     }
 }
