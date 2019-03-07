db9009caa5201a01cce20f21a229f9fc5d0f01eb
IHOR PETROV <Basicprof@gmail.com>
2019-03-07 21:16:08 +0200

    Lesson09 HW08

--- src/ru/javawebinar/basejava/util/DateUtil.java
+++ src/ru/javawebinar/basejava/util/DateUtil.java
@@ -8,8 +8,10 @@ import java.time.Month;
  * 20.07.2016
  */
 public class DateUtil {
-    public static LocalDate of(int year, Month month) {
 
+    public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
+
+    public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
     }
 }
