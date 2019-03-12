5e571f2f7f240d74df33f97bd166e8f295baa1f6
IHOR PETROV <Basicprof@gmail.com>
2019-03-12 18:11:15 +0200

    Lesson15 HW14

---
+++ config/populateContact.sql
@@ -0,0 +1,3 @@
+INSERT INTO contact (resume_uuid, type, value) VALUES
+  ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'PHONE','380505132950'),
+  ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'SKYPE', 'ip500800') ;
